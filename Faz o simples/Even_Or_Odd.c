e0fc7f2bcd79d53c98679155f177eb296cd15818
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-02-19 16:22:38 -0300

    Even or Odd

---
+++ Faz o simples/Even_Or_Odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+
+int num = 2457 ;
+    if(num % 2 == 0) {
+        printf("%d is even", num) ;
+    } else {
+        printf("%d is odd", num) ;
+    }
+
+
+    return 0;
+
+}
