643c317cc528f8698cc5097c3d2d6eab53fa7fa8
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-02-20 13:03:12 -0300

    noting

--- Faz o simples/Even_Or_Odd.c
+++ Faz o simples/Even_Or_Odd.c
@@ -5,7 +5,9 @@
 int main(){
 
 int num = 2457 ;
-    if(num % 2 == 0) {
+    if(num % 2 == 0) { /*o % não é literalmente porcentagem, mas sim o operador de módulo, que retorna o    
+                        resto da divisão entre dois números. */
+   
         printf("%d is even", num) ;
     } else {
         printf("%d is odd", num) ;
