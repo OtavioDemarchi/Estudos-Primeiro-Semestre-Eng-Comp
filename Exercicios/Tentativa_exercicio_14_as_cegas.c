e6e47627f96404a5dfc28218f79f694f96f8e117
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-02-23 15:59:28 -0300

    aaaaaaa

---
+++ Exercicios/Tentativa_exercicio_14_as_cegas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+int idade;
+printf("Digite a sua idade\n");
+scanf("%d", &idade);
+if(idade <= 12) {
+printf("Voce tem %d e eh uma crianca\n"), idade;}
+else if (idade <= 19) {
+printf("Voce tem %d e eh um adolescente\n", idade);}
+else if (idade <= 60) //por algum motivo eu coloquei um ; aqui fazendo com que o else ficasse sem esse if, melhore Tatah
+{
+printf("Voce tem %d e eh um adulto\n", idade);}
+else 
+printf("Voce tem %d e em um idoso \n", idade);
+return 0;
+}
---
+++ Exercicios/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+    printf("Voce tem %d e eh uma criança\n", idade);}
+
+    if(idade <= 19){
+
+    printf("Voce tem %d e eh um adolescente\n", idade);}
