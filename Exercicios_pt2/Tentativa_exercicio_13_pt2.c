a1bf875f96655f3f9e077e27494ab193b5b65b8c
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-03-06 03:10:39 -0300

    terminei amem

--- Exercicios_pt2/Tentativa_exercicio_13_pt2.c
+++ Exercicios_pt2/Tentativa_exercicio_13_pt2.c
@@ -10,13 +10,16 @@ int n, soma = 0;
 
 
 printf("insira quantos quantos numeros voce gostaria de somar:\n");
+scanf("%d", &somaquant);
 
 
 for(i = 0; i < somaquant; i++){
 printf("insira o %do numero:\n", i+1);
-scanf("%d", n);
+scanf("%d", &n);
 soma += n;
 }
+
+printf("\nO resultado eh: %d", soma);
 
     return 0;
 }
