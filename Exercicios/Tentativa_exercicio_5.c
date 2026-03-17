772bc162b3c799b85d6ffe2e5b8ee5a067500d44
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-03-17 04:21:17 -0300

    arrumei exercicios 5 e 6

--- Exercicios/Tentativa_exercicio_5.c
+++ Exercicios/Tentativa_exercicio_5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>   
+#include <math.h>
 
 int main (){
 
-//6 - Construa um programa para mostrar somente números impares;
-
-//AS QUESTÕES 5 E 6 ESTÃO TROCADAS VOU ARRUMAR ASSIM QUE POSSIVEL
+//5 - Construa um programa para mostrar somente números pares;
 
 int num1, num2, num3, num4, num5 = 0;
 
@@ -14,28 +12,29 @@ printf("Insira cinco numeros: ");
 
 scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
 
-printf("xX NUMEROS IMPARES Xx\n");
+printf("xX NUMEROS PARES Xx\n");
 
-    if(num1 % 2 != 0) { //!= indica "diferente de, nesse caso seria "se o resultado for diferente de 0"
+    if(num1 % 2 == 0) {
     printf("%d\n", num1);
     }
 
-    if(num2 % 2 != 0) {
+    if(num2 % 2 == 0) {
     printf("%d\n", num2);
     }
 
-    if(num3 % 2 != 0) {
+    if(num3 % 2 == 0) {
     printf("%d\n", num3);
     }
 
-    if(num4 % 2 != 0) {
+    if(num4 % 2 == 0) {
     printf("%d\n", num4);
     }
 
-    if(num5 % 2 != 0) {
+    if(num5 % 2 == 0) {
     printf("%d\n", num5);
     }
     
 
     return 0;
 }
+
--- Exercicios/Tentativa_exercicio_6.c
+++ Exercicios/Tentativa_exercicio_6.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <math.h>   
 
 int main (){
 
-//5 - Construa um programa para mostrar somente números pares;
+//6 - Construa um programa para mostrar somente números impares;
 
 int num1, num2, num3, num4, num5 = 0;
 
@@ -12,25 +13,25 @@ printf("Insira cinco numeros: ");
 
 scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
 
-printf("xX NUMEROS PARES Xx\n");
+printf("xX NUMEROS IMPARES Xx\n");
 
-    if(num1 % 2 == 0) {
+    if(num1 % 2 != 0) { //!= indica "diferente de, nesse caso seria "se o resultado for diferente de 0"
     printf("%d\n", num1);
     }
 
-    if(num2 % 2 == 0) {
+    if(num2 % 2 != 0) {
     printf("%d\n", num2);
     }
 
-    if(num3 % 2 == 0) {
+    if(num3 % 2 != 0) {
     printf("%d\n", num3);
     }
 
-    if(num4 % 2 == 0) {
+    if(num4 % 2 != 0) {
     printf("%d\n", num4);
     }
 
-    if(num5 % 2 == 0) {
+    if(num5 % 2 != 0) {
     printf("%d\n", num5);
     }
     
